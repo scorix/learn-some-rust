5819351e442c7335c2ef672b871e83127d929ceb
scorix <scorix@gmail.com>
2016-06-22 20:56:58 +0800

    get a hash key

--- src/exercise2.rs
+++ src/exercise2.rs
@@ -1,5 +1,6 @@
 extern crate redis;
 use redis::{Commands};
+use std::collections::HashMap;
 
 // connect to redis
 fn establish_connection() -> redis::Connection {
@@ -7,8 +8,7 @@ fn establish_connection() -> redis::Connection {
     client.get_connection().unwrap()
 }
 
-fn set_and_get<T: redis::ToRedisArgs>(con: &redis::Connection, key: &str, value: T) {
-    let _ : () = con.set(key, value).unwrap();
+fn get(con: &redis::Connection, key: &str) {
     let value : Option<redis::Value> = con.get(key).unwrap();
     let ttl : i64 = redis::cmd("TTL").arg(key).query(con).unwrap();
     println!("key: {k:?}, value: {v:?}, ttl: {ttl:?}", k = key, v = value, ttl = ttl);
@@ -21,12 +21,13 @@ fn get_a_random_key(con: &redis::Connection) {
 }
 
 fn get_a_missing_key(con: &redis::Connection, key: &str) {
-    let value : Option<String> = con.get(key).unwrap();
+    let value : Option<redis::Value> = con.get(key).unwrap();
     println!("key: {k:?}, value: {v:?}", k = key, v = value);
 }
 
-fn get_a_hash_key(con: &redis::Connection) {
-    unimplemented!()
+fn get_a_hash_key(con: &redis::Connection, key: &str) {
+    let value : HashMap<String, redis::Value> = redis::cmd("HGETALL").arg(key).query(con).unwrap();
+    println!("{:?}", value);
 }
 
 fn get_a_set(con: &redis::Connection) {
@@ -36,8 +37,15 @@ fn get_a_set(con: &redis::Connection) {
 fn main() {
     let con : redis::Connection = establish_connection();
     let _ : () = redis::cmd("FLUSHALL").query(&con).unwrap();
-    set_and_get(&con, "a", 43);
-    set_and_get(&con, "b", "foo");
+    let _ : () = con.set("a", 43).unwrap();
+    let _ : () = con.set("b", "foo").unwrap();
+
+    get(&con, "a");
+    get(&con, "b");
     get_a_random_key(&con);
     get_a_missing_key(&con, "missing_key");
+
+    let _ : () = redis::cmd("HMSET").arg("hash_key").arg("foo").arg("bar").arg("size").arg(1).query(&con).unwrap();
+    get_a_hash_key(&con, "hash_key");
+    get_a_hash_key(&con, "missing_hash");
 }
