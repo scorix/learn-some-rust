39c59042b1e57036b1cbcb3ba70f6332718c90f3
scorix <scorix@gmail.com>
2016-06-22 20:41:17 +0800

    get a missing key

--- src/exercise2.rs
+++ src/exercise2.rs
@@ -9,7 +9,7 @@ fn establish_connection() -> redis::Connection {
 
 fn set_and_get<T: redis::ToRedisArgs>(con: &redis::Connection, key: &str, value: T) {
     let _ : () = con.set(key, value).unwrap();
-    let value : redis::Value = con.get(key).unwrap();
+    let value : Option<redis::Value> = con.get(key).unwrap();
     let ttl : i64 = redis::cmd("TTL").arg(key).query(con).unwrap();
     println!("key: {k:?}, value: {v:?}, ttl: {ttl:?}", k = key, v = value, ttl = ttl);
 }
@@ -20,10 +20,24 @@ fn get_a_random_key(con: &redis::Connection) {
     println!("random key is {}, its value is {}.", random_key, value);
 }
 
+fn get_a_missing_key(con: &redis::Connection, key: &str) {
+    let value : Option<String> = con.get(key).unwrap();
+    println!("key: {k:?}, value: {v:?}", k = key, v = value);
+}
+
+fn get_a_hash_key(con: &redis::Connection) {
+    unimplemented!()
+}
+
+fn get_a_set(con: &redis::Connection) {
+    unimplemented!()
+}
+
 fn main() {
     let con : redis::Connection = establish_connection();
     let _ : () = redis::cmd("FLUSHALL").query(&con).unwrap();
     set_and_get(&con, "a", 43);
     set_and_get(&con, "b", "foo");
     get_a_random_key(&con);
+    get_a_missing_key(&con, "missing_key");
 }
